2f1277e7df40ea3c3cdb5e1a3eca436a855440fe
Long N <lnguyen30@uco.edu>
2022-07-08 22:44:34 -0500

    created posts method

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from turtle import title
+from fastapi import Body, FastAPI
 
 app = FastAPI()
 
@@ -11,3 +12,9 @@ def root():
 @app.get("/posts")
 def get_posts():
     return {"data": "this is a post"}
+
+#body is the json output converted into a dictionary, stored into Payload var
+@app.post("/create")
+def create_posts(payLoad: dict = Body(...)):
+   print(payLoad)
+   return {"new_post": f"title {payLoad['title']}, content {payLoad['content']}" }
