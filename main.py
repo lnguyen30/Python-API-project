deb70baf288c1cd2b324f9075359c67bcd9bd88a
Long N <lnguyen30@uco.edu>
2022-07-10 14:32:56 -0500

    changed object to dict

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from turtle import title
+from typing import Optional
 from fastapi import Body, FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int]= None
 
 # request for get method, url: "/"
 @app.get("/")
@@ -13,8 +21,9 @@ def root():
 def get_posts():
     return {"data": "this is a post"}
 
-#body is the json output converted into a dictionary, stored into Payload var
-@app.post("/create")
-def create_posts(payLoad: dict = Body(...)):
-   print(payLoad)
-   return {"new_post": f"title {payLoad['title']}, content {payLoad['content']}" }
+#body is the json output converted into a dictionary, Post object stored into new_post var 
+@app.post("/createposts")
+def create_posts(post: Post):
+   print(post)
+   print(post.dict()) #convert to dict 
+   return {"data": post }
