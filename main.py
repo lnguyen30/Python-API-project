7d78bcbadce82be93d5119776ea5a883437219a7
Long N <lnguyen30@uco.edu>
2022-07-17 19:22:00 -0500

    added get id methods

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from turtle import title
 from typing import Optional
 from fastapi import Body, FastAPI
 from pydantic import BaseModel
+from random import randrange
 
 app = FastAPI()
 
@@ -11,19 +12,35 @@ class Post(BaseModel):
     published: bool = True
     rating: Optional[int]= None
 
+#dictionary of post objects
+my_posts = [{"title": "title of post 1", "content": "content of post 1", "id" : 1},{"title": "title of post 2", "content": "content of post 2", "id" : 2}]
+
 # request for get method, url: "/"
 @app.get("/")
 def root():
     return{"message": "Welcome!!"}
 
+#find specific post 
+def find_post(id):
+    for p in my_posts:
+        if p["id"] == id:
+            return p
 
 @app.get("/posts")
 def get_posts():
-    return {"data": "this is a post"}
+    return {"data": my_posts}
 
 #body is the json output converted into a dictionary, Post object stored into new_post var 
-@app.post("/createposts")
+@app.post("/posts")
 def create_posts(post: Post):
-   print(post)
-   print(post.dict()) #convert to dict 
-   return {"data": post }
+     post_dict = post.dict()
+     post_dict['id'] = randrange(0, 10000000)
+     my_posts.append(post_dict)
+#    print(post)
+#    print(post.dict()) #convert to dict 
+     return {"data": post_dict }
+
+@app.get("/posts/{id}")
+def get_post(id):
+    post = find_post(int(id))
+    return{"post_detail": post}
